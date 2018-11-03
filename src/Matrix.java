f128086f303ef1558839b3f7373a0ca8d4ac368c
Yassien <>
2018-11-03 02:38:13 +0200

    Moved the reduce method from Matrix to OperationsManager
    Both Docx and client logging now work when using the reduce method

--- src/Matrix.java
+++ src/Matrix.java
@@ -51,81 +51,12 @@ public class Matrix{
 		}
 	}
 	//checks if that row is an all zero row
-	public boolean isAllZero(int row){
+	public boolean isZeroRow(int row){
 		for(double d : matrix[row-1]){
 			if(d != 0){
 				 return false;
 			}
 		}
 		return true;
-	}
-	
-	//automatically reduces a double matrix to reduced row form
-	public void reduce(){
-		//ui.printStringToConsole("Original Matrix");
-		//ui.printMatrixToConsole(matrix);
-		//add a condition for matrices that have more rows than columns, so that the loop doesn't exceed the smallest value
-		
-		
-		//loops diagonally through the pivot points and operates on them to be == 1
-		for(int i = 0; i < matrix.length; i++){
-			//if diagonal entry is 0 AND the row under it isn't an all zero row(because all zero rows have to stay in the bottom),
-			//switch that row with the row under it
-			//added a check to make sure the row under current row isn't out of bounds
-			if(i+2 < matrix.length){
-				if(matrix[i][i] == 0 && !isAllZero(i+2)){
-					swapRowWithRow(i+1, i+2);
-				}
-			}
-			//System.out.println(i);
-			if(matrix[i][i] != 1 && matrix[i][i] != 0){	
-				//before changing the pivot value to 1, check if there are any other rows under it with 1 or -1 in the current column,
-				//IF there is, switch that row with the current row
-				for(int j = i+1; j < matrix.length; j++){
-					if(matrix[j][i] == 1 || matrix[j][i] == -1){
-						swapRowWithRow(i+1, j+1);
-						break;
-					}
-				}
-				//add one because the function takes row number NOT the array index (row number > 0)
-				//add a condition to only do this if it isn't == 1 (since the goal is to change it to 1)
-				//we add this because sometimes the previous operation switches the pivot row to one that already has a 1
-				//therefore operating on it would be a useless operation
-				if(matrix[i][i] != 1){
-					multiplyRow(i+1, 1 / matrix[i][i]);
-				}
-				
-			}
-			//for each pivot point, we zero the values above/under it, after turning it to 1 (previous loop)
-			for(int k = 0; k < matrix.length; k++){
-				if(matrix[k][i] != 0 && k != i && matrix[i][i] != 0){	
-					addMultipleOfRowToRow(i+1, k+1, matrix[k][i] * -1);
-				}		
-			}	
-		}
-		//ui.printStringToConsole("Running Back Check..");
-		//run checks to make sure matrix form is correct
-		//iterate through the rows from bottom to top, if the row above is a zero row, switch current row with it
-		for(int i = matrix.length; i > 1; i--){
-			if(isAllZero(i-1)){
-				swapRowWithRow(i, i-1);
-			}
-		}
-		//For each non zero row, if the first entry in it isn't one, change it to one and zero the values under/above it using it
-		for(int i = 0; i < matrix.length; i++){
-			for(int j = 0; j < matrix[i].length; j++){
-				if(matrix[i][j] == 1){
-					break;
-				}else if(!isAllZero(i+1) && matrix[i][j] != 0){
-					multiplyRow(i+1, 1 / matrix[i][j]);
-					for(int k = 0; k < matrix.length; k++){
-						if(matrix[k][j] != 0 && matrix[k][j] != 1){	
-							addMultipleOfRowToRow(i+1, k+1, matrix[k][j] * -1);
-						}		
-					}
-				}		
-			}
-		}
-		//ui.printStringToConsole("Done!");
 	}
 }
--- src/OperationsManager.java
+++ src/OperationsManager.java
@@ -37,7 +37,7 @@ public class OperationsManager{
 			toDocx(String.format("R%d + R%d", row1, row2));
 		}else{
 			ui.printStringToConsole("Multipy Row " + row1 + " By " + (int)value + " and add it to Row " + row2);
-			toDocx(String.format("R%d x R%s + R%d", row1, String.valueOf(value), row2));
+			toDocx(String.format("R%d x %s + R%d", row1, String.valueOf(value), row2));
 		}
 		ui.printMatrixToConsole(matrix);
 		ui.update(matrix);
@@ -46,12 +46,13 @@ public class OperationsManager{
 		updateMatrix(ui.getInputMatrix());
 		if(value % 1 == 0){
 			ui.printStringToConsole("Multipy Row " + row + " By " + (int)value);
+			toDocx(String.format("R%d x %s", row, String.valueOf(value)));
 		}else{
 			ui.printStringToConsole("Multipy Row " + row + " By " + Fraction.valueOf(value));
+			toDocx(String.format("R%d x %s", row, Fraction.valueOf(value)));
 		}	
 		matrix.multiplyRow(row, value);
 		ui.printMatrixToConsole(matrix);
-		toDocx(String.format("R%d x %s", row, String.valueOf(value)));
 		ui.update(matrix);
 	}
 	public void swapRowWithRow(int row1, int row2){
@@ -62,10 +63,73 @@ public class OperationsManager{
 		toDocx(String.format("R%d <-> R%d", row1, row2));
 		ui.update(matrix);
 	}
+	//automatically reduces a double matrix to reduced row form
 	public void reduce(){
 		updateMatrix(ui.getInputMatrix());
-		matrix.reduce();
-		ui.update(matrix);
+		ui.printStringToConsole("Original Matrix");
+		ui.printMatrixToConsole(matrix);
+		toDocx("Original Matrix");
+		//add a condition for matrices that have more rows than columns, so that the loop doesn't exceed the smallest value
+		
+		//loops diagonally through the pivot points and operates on them to be == 1
+		for(int i = 0; i < matrix.getRows(); i++){
+			//if diagonal entry is 0 AND the row under it isn't an all zero row(because all zero rows have to stay in the bottom),
+			//switch that row with the row under it
+			//added a check to make sure the row under current row isn't out of bounds
+			if(i+2 < matrix.getRows()){
+				if(matrix.getValue(i, i) == 0 && !matrix.isZeroRow(i+2)){
+					swapRowWithRow(i+1, i+2);
+				}
+			}
+			if(matrix.getValue(i, i) != 1 && matrix.getValue(i, i) != 0){	
+				//before changing the pivot value to 1, check if there are any other rows under it with 1 or -1 in the current column,
+				//IF there is, switch that row with the current row
+				for(int j = i+1; j < matrix.getRows(); j++){
+					if(matrix.getValue(j, i) == 1 || matrix.getValue(j, i) == -1){
+						swapRowWithRow(i+1, j+1);
+						break;
+					}
+				}
+				//add one because the function takes row number NOT the array index (row number > 0)
+				//add a condition to only do this if it isn't == 1 (since the goal is to change it to 1)
+				//we add this because sometimes the previous operation switches the pivot row to one that already has a 1
+				//therefore operating on it would be a useless operation
+				if(matrix.getValue(i, i) != 1){
+					multiplyRow(i+1, 1 / matrix.getValue(i, i));
+				}
+				
+			}
+			//for each pivot point, we zero the values above/under it, after turning it to 1 (previous loop)
+			for(int k = 0; k < matrix.getRows(); k++){
+				if(matrix.getValue(k, i) != 0 && k != i && matrix.getValue(i, i) != 0){	
+					addMultipleOfRowToRow(i+1, k+1, matrix.getValue(k, i) * -1);
+				}		
+			}	
+		}
+		//ui.printStringToConsole("Running Back Check..");
+		//run checks to make sure matrix form is correct
+		//iterate through the rows from bottom to top, if the row above is a zero row, switch current row with it
+		for(int i = matrix.getRows(); i > 1; i--){
+			if(matrix.isZeroRow(i-1)){
+				swapRowWithRow(i, i-1);
+			}
+		}
+		//For each non zero row, if the first entry in it isn't one, change it to one and zero the values under/above it using it
+		for(int i = 0; i < matrix.getRows(); i++){
+			for(int j = 0; j < matrix.getColumns(); j++){
+				if(matrix.getValue(i, j) == 1){
+					break;
+				}else if(!matrix.isZeroRow(i+1) && matrix.getValue(i, j) != 0){
+					multiplyRow(i+1, 1 / matrix.getValue(i, j));
+					for(int k = 0; k < matrix.getRows(); k++){
+						if(matrix.getValue(k, j) != 0 && matrix.getValue(k, j) != 1){	
+							addMultipleOfRowToRow(i+1, k+1, matrix.getValue(k, j) * -1);
+						}		
+					}
+				}		
+			}
+		}
+		ui.printStringToConsole("Done!");
 	}
 	public void setDocxFile(File f){
 		new Thread("Non EDT"){
